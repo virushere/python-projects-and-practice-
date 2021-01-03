901405d7cc97d3014e4f2f7b6f5c6ed9d061176e
Yash Vyas <56440961+virushere@users.noreply.github.com>
2021-01-03 23:46:55 +0530

    Add files via upload

---
+++ Madlips.py
@@ -0,0 +1,8 @@
+color = input("Enter a color: ")
+friend = input("Enter a friends name: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are", color)
+print(friend, "is my friend")
+print("violets are blue")
+print("I love", celebrity)
